1392348ec5263e41fd7b2a83ea1a1835384e0712
italolopes9 <it-alo19@hotmail.com>
2020-09-07 20:29:11 -0300

    Finalizado listagem de usuário

---
+++ knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: "knex_teste",
+      user: "postgres",
+      password: "postgres"
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: `${__dirname}/src/database/migrations`
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`
+    }
+  }
+};
