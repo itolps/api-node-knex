a267912930d6aa2bbd87e3dfaa6d56364d98f714
italolopes9 <it-alo19@hotmail.com>
2020-09-07 21:11:32 -0300

    Finalizado Crud usuários

--- src/server.js
+++ src/server.js
@@ -3,6 +3,21 @@ const routes = require('./routes');
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
+
+//notFound
+app.use((req, res, next) => {
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+
+// catch all (capturar todos os erros)
+app.use((error, req, res, next) => {
+    res.status(erros.status || 500)
+    res.json({ error: error.message })
+
+});
 
 app.listen(3333, () => console.log('Server rodando'));
